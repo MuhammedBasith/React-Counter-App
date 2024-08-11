ed78c89c814b1a1138a1f397048586daa2b55e05
MuhammedBasith <iambasith123@gmail.com>
2024-08-11 16:53:48 +0300

    fix mysql error

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ var mysql = require('mysql2');
 require('dotenv').config();
 
 var db = mysql.createConnection({
-    uri: process.env.MYSQL_URI,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
 });
 
 db.connect((err) => {
